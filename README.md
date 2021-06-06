fdc1d1626618b71197a2ef76facf6bbdafd82bc1
James Brunskill <james.brunskill@gmail.com>
2021-06-06 16:55:25 +1200

    Added feature to remove " quotes from input

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A quick hack bcrypt for the commandline
 
+It reads a password via standard in and returns a bcrypt hash using default settings to standard out.
+
+Note: bcrypt removes leading and trailing quote characters e.g. " as these are often accidentally added on Windows platforms
+
 __usage examples__
 
 `pwgen -s -1 14 | bcrypt`
--- main.go
+++ main.go
@@ -6,6 +6,17 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+//cleanString removes leading whitespace and quotes (as often get added when running as a commandline app in windows)
+func cleanString(s string) string {
+	if len(s) > 0 && s[0] == '"' {
+		s = s[1:]
+	}
+	if len(s) > 0 && s[len(s)-1] == '"' {
+		s = s[:len(s)-1]
+	}
+	return s
+}
+
 func main() {
 
 	var pwd string
@@ -14,6 +25,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	pwd = cleanString(pwd)
 
 	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
 	if err != nil {
